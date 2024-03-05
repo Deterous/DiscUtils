5a32a16ab3d95c5aa6c551e9d7b82ad9321415b4
Deterous <138427222+Deterous@users.noreply.github.com>
2024-03-05 17:33:20 +0900

    Better framework gating

--- Library/DiscUtils.Core/CoreCompat/EncodingHelper.cs
+++ Library/DiscUtils.Core/CoreCompat/EncodingHelper.cs
@@ -1,4 +1,4 @@
-﻿#if !NET40 && !NET45
+﻿#if NETCOREAPP
 using System.Text;
 #endif
 
@@ -15,7 +15,7 @@ namespace LibIRD.DiscUtils.CoreCompat
 
             _registered = true;
 
-#if !NET40 && !NET45
+#if NETCOREAPP
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 #endif
         }
